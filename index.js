16840adb1e341507538e292811d80f401da5734a
Stephen Hendrix <stephenth12@gmail.com>
2017-12-29 23:01:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
-function yes(string) {return
-function sayHiToGrandma(string) {return string.make = "YES INDEED!"()}
+function sayHiToGrandma(string) {return 
