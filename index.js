be64c478a708184142de5f3ba65333a03ef59f11
Stephen Hendrix <stephenth12@gmail.com>
2017-12-29 23:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 
 function sayhiToGrandma(string) {
-  return
+  if (string === "hello")
+}
