4dd01008f516739be4cd99cdb1e37fa1981645f6
Stephen Hendrix <stephenth12@gmail.com>
2017-12-29 23:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
-function sayHiToGrandma(STRING) {return ("YES INDEED!")}
 
+var hello = "hello"
+var HELLO = "HELLO"
+var love = "I love you, Grandma."
+
+function sayHiToGrandma(HELLO) {return ("YES INDEED!")}
+function sayHiToGrandma(hello) {return ("I can't hear you!")}
+function sayHitoGrandma(love) {return ("I love you, too.")}
