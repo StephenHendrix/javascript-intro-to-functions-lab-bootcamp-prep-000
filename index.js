b870d3762a3f150548b279bfab05440386ec2236
Stephen Hendrix <stephenth12@gmail.com>
2017-12-29 23:47:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 
 function sayhiToGrandma(string) {
-  if (string.toLowerCase() === "lowercase") {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
 }
